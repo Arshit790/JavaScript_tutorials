f7626dc6b2b123c1c86e560ef67ee85e25112954
Arshit790 <arshitdixit5@gmail.com>
2024-08-17 20:00:00 +0530

    "datatypes"

---
+++ Tutorial2.js
@@ -0,0 +1,31 @@
+/* Data Types */
+
+"use strict"; // treat all js as newer JS version (it cant be disabled again)
+
+// alert(3 + 3); we are using NOdejs not browser
+
+console.log(3 + 3);
+console.log("hello");
+
+let name = "abc";
+let age = 15;
+let isLogggedIn = false;
+let state = null;
+
+/* Types */
+//number => 2 to power 53 (range memory address)
+// BigInt
+// string => ""
+// Boolean
+// null => standalone value (Empty value and can be assigned too a variables )
+// unefined => when value is not assigned to the variables
+// symbol => unique
+
+//object
+
+/* NULL IS AN OBJECT */
+
+console.log(typeof "hello");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
