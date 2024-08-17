7025ae273bd505fb2c6c9cc38cb83a63d08556e0
Arshit790 <arshitdixit5@gmail.com>
2024-08-17 19:34:50 +0530

    "let ,var and const"

--- test.js
+++ test.js
@@ -1,2 +1,29 @@
-const accountID = 234455
-let accountEmail = "xyz@gmail.com"
+/*  LET, VAR AND CONST  */
+
+const accountID = 234455;
+let accountEmail = "xyz@gmail.com"; // resolve scope issues
+var accountPassword = "12344";
+
+/* Var keyword doesnt support block scope and functional scope*/
+
+accountCity = "Mathura";
+let accountState; // gives undefined value when its value not declared
+
+/* const keyword doesnt change its value(it locked the variables)
+ accountID = 2 (can't change its value - not allowed)
+ */
+
+console.log(accountID);
+
+accountEmail = "sdf@g.com";
+accountPassword = "23456";
+accountCity = "BLR";
+
+console.table([
+  accountID,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
+
