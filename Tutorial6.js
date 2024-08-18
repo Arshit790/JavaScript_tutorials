87d60434c8b0b5c0474b88b012d045595598ded9
Arshit790 <arshitdixit5@gmail.com>
2024-08-18 18:25:31 +0530

    "Primitive and non primitive"

---
+++ Tutorial6.js
@@ -0,0 +1,24 @@
+//How we put data into memory and want to access it 
+//! Primitive and non-primitive
+
+
+// 7types of Primitive(Call by value) : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId) // false
+
+//  Non - Primitive (Call by reference) : Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+
+let myObj = {
+  name : "arshit",
+  age : 22
+}
+
+const myFunction = function(){
+console.log("Hello!");
+}
+
+console.log(typeof myFunction)
