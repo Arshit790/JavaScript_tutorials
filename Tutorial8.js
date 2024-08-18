70ffb6c667d11153d1bb368a94a58d04bd227053
Arshit790 <arshitdixit5@gmail.com>
2024-08-18 19:04:46 +0530

    "String  Operations"

---
+++ Tutorial8.js
@@ -0,0 +1,34 @@
+// Number and Maths
+
+const name = "Arshit"
+const counter = 50
+// console.log(name + counter + " Value")
+
+
+console.log(`Hello my namme is ${name} and repo count ${counter}`)
+
+const gameName = new String('luckylp-lp-com')
+console.log(gameName[0])
+console.log(gameName.__proto__)
+
+
+console.log(gameName.length)
+console.log(gameName.concat('.exe'))
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(3))
+
+
+const newStr = gameName.substring(0,4)
+console.log(newStr)
+
+const anotherStr = gameName.slice(-8,4)
+console.log(anotherStr)
+
+const strNew = "    lucky   "
+console.log(strNew)
+console.log(strNew.trim())
+
+const url = "https://arshit.com/arshit%20dixit"
+console.log(url.replace('%20','-'))
+
+console.log(gameName.split('-'))
