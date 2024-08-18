861ff3b69c0cd8afb63d5f0cc7b214cba65a18d5
Arshit790 <arshitdixit5@gmail.com>
2024-08-18 19:32:28 +0530

    "num and math"

---
+++ Tutorial9.js
@@ -0,0 +1,35 @@
+// Date and Time
+
+const score = 400
+// console.log(score)
+
+const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+const num = 1123.8966
+// console.log(num.toPrecision(3))
+
+const otherNum = 1000000
+// console.log(otherNum.toLocaleString('en-IN'))
+
+// +++++++++++++++   Maths    +++++++++++++++++++
+
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9))
+// console.log(Math.sqrt(16))
+// console.log(Math.min(1,2,3,5,6))
+
+
+console.log(Math.random() )
+console.log((Math.random() * 10) + 1)
+// console.log(Math.floor(Math.random() * 10) + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min )
