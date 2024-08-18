bbd97077a0146e05d4fdf0ec7df45387a325f294
Arshit790 <arshitdixit5@gmail.com>
2024-08-18 18:08:33 +0530

    "datatypes"

---
+++ Tutorial5.js
@@ -0,0 +1,22 @@
+// Comparisons
+
+// ? console.log(2 > 1)
+// ? console.log(2 >= 1)
+// ? console.log(2 < 1)
+// ? console.log(2 == 1)
+// ? console.log(2 != 1)
+
+// console.log("2" > 1)  // !string converts to number
+// console.log("02" > 1)
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0) // !comparison converts null to 0
+
+// console.log(undefined == 0)
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+
+//! === (value as well as datatype)
+console.log("2" === 2)
+console.log("2" == 2)
