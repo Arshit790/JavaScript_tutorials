1f2ae32160b9ee9958f2be6302d50426d599dc3a
Arshit790 <arshitdixit5@gmail.com>
2024-08-22 12:07:30 +0530

    "Function with objects and arrays"

---
+++ Tutorial17.js
@@ -0,0 +1,30 @@
+function calculateCartPrice(val1,val2,...num1){ //rest operator ...
+  return num1
+}
+
+// console.log(calculateCartPrice(200,400,500,700))
+
+
+ // ! ... rest == spread (Depends on usuability)
+
+const user = {
+  username : "Lucky",
+  prices : "999"
+}
+function handleObject(anyobject){
+  
+  console.log(`Username is ${anyobject.username} and price is ${anyobject.price}` )
+}
+// handleObject(user)
+handleObject({
+  username :"sam",
+  price : "399"
+})
+
+const myAr = [200,400,100,600]
+
+function returSecond(getArray){
+  return getArray[2]
+}
+// console.log(returSecond(myAr))
+console.log(returSecond([100,300,500,400]))
