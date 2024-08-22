13803f40350893ba65df353c97701ad754d6e2ab
Arshit790 <arshitdixit5@gmail.com>
2024-08-22 11:43:11 +0530

    "objects_pt2"

---
+++ Tutorial15.js
@@ -0,0 +1,28 @@
+ // objects destructuring 
+
+const course = {
+  coursename : "js in hindi",
+  price : "999",
+  courseInstructor : "lucky"
+}
+// course.corserInstructor
+
+const {courseInstructor : instructor} = course
+// console.log(courseInstructor);
+console.log(instructor);
+
+
+//? +++++++++++++++++++++ API +++++++++++++++++++++++++++++++
+
+// in json keys and valuees always in string
+//  ! {
+//  ! "coursename" : "js in hindi",
+//  ! "price" : "free",
+//  !"name" : "lucky"
+//  ! }
+ 
+// [
+//   {},
+//   {},
+//   {}
+// ]
