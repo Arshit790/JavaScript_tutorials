2443b483df3e51dbfd6c89f42b11a75e429d3ab6
Arshit790 <arshitdixit5@gmail.com>
2024-08-31 19:04:05 +0530

    "Prototype and user Methods"

--- CLasses_OOPS/prototype.js
+++ CLasses_OOPS/prototype.js
@@ -18,4 +18,48 @@ Object.prototype.lucky = function () {
   console.log(`Lucky is present in all objects`);
 };
 
-heroPower.lucky();
+Array.prototype.heyLucky = function () {
+  console.log(`Hello Lucky`);
+};
+
+// heroPower.lucky();
+// myHeros.lucky();
+
+// myHeros.heyLucky();
+// heroPower.heyLucky()  because we can access from parents not from child to parent
+
+// ! Inheritance
+
+const User = {
+  name: "Lukcy",
+  email: "abc@g.com",
+};
+
+const Teacher = {
+  makeVideo: true,
+};
+
+const TeachingSupport = {
+  isAvailable: false,
+};
+
+const TASupport = {
+  makeAssignment: "JS Assignment",
+  fullTime: true,
+  __proto__: TeachingSupport,
+};
+Teacher.__proto__ = User;
+
+// modern syntax of Inheritance
+
+Object.setPrototypeOf(TeachingSupport,Teacher)
+
+
+let anotherUsername = "lucky      "
+String.prototype.trueLength = function(){
+    console.log(`${this}`)
+    console.log(`True Length is: ${this.trim().length}`)
+}
+anotherUsername.trueLength()
+"luck".trueLength()
+"iceTea".trueLength()
