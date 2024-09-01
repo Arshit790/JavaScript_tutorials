d714284701525f83c05819e41c912371c06ef3c7
Arshit790 <arshitdixit5@gmail.com>
2024-09-01 17:17:05 +0530

    "call() method"

--- CLasses_OOPS/call.js
+++ CLasses_OOPS/call.js
@@ -1,9 +1,14 @@
 function setUsername(username){
     // complex DB calls
     this.username = username
+    console.log("Called")
 }
 function createUser(username,email,password){
-    setUsername(username)
+    setUsername.call(this,username)
+    
     this.email = email
     this.password = password
 }
+
+const chai = new createUser('chai','chai@b.com',"123")
+console.log(chai)
